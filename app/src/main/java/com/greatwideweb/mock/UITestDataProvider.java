5f764ea64a765b9041c005c61251225a241ed6c3
Dave Turner <davetii@gmail.com>
2016-05-07 20:44:40 -0400

    5.7.2016

---
+++ app/src/main/java/com/greatwideweb/mock/UITestDataProvider.java
@@ -0,0 +1,7 @@
+package com.greatwideweb.mock;
+
+/**
+ * Created by dave on 5/6/2016.
+ */
+public class UITestDataProvider {
+}
---
+++ app/src/test/java/com/greatwideweb/youtube/mock/UITestDataProviderTest.java
@@ -0,0 +1,7 @@
+package com.greatwideweb.youtube.mock;
+
+/**
+ * Created by dave on 5/6/2016.
+ */
+public class UITestDataProviderTest {
+}
